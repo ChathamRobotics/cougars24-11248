7d801b4acebd5c3010b80b8adae622182792205d
Thaddeus Kobylarz <tjmccrafter101@gmail.com>
2023-10-19 10:47:40 -0400

    Roadrunner Updated with ramp up and ramp down fine tuners

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTestAdvancedMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTestAdvancedMecanum.java
@@ -4,6 +4,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
@@ -18,6 +19,9 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class LocalizationTestAdvancedMecanum extends LinearOpMode {
 
     private float Motorpower = 0.4f;
+    private final ElapsedTime runtime = new ElapsedTime();
+    private double lastPowerChangeTime;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -31,36 +35,46 @@ public class LocalizationTestAdvancedMecanum extends LinearOpMode {
             drive.setWeightedDrivePower(
                     new Pose2d(
                             -gamepad1.left_stick_y * Motorpower,
-                            -gamepad1.left_stick_x * Motorpower,
+                            -(gamepad1.right_trigger - gamepad1.left_trigger + gamepad1.left_stick_x) * Motorpower * 1.2,
                             -gamepad1.right_stick_x * Motorpower
                     )
             );
-
-            drive.update();
-
-            // Quick Adjust Power
-            if (gamepad1.y) {
-                Motorpower = 0.8f;
-            } else if (gamepad1.a) {
-                Motorpower = 0.4f;
+            // Ramp up & Ramp Down
+            if (runtime.time() > lastPowerChangeTime + 0.5) {
+                if (gamepad1.dpad_down) {
+                    Motorpower = Math.max(0, Motorpower - 0.1f);
+                    lastPowerChangeTime = runtime.time();
+                } else if (gamepad1.dpad_up) {
+                    Motorpower = Math.min(1, Motorpower + 0.1f);
+                    lastPowerChangeTime = runtime.time();
+                }
             }
-            if (gamepad1.left_bumper) {
-                Motorpower = 0.9f;
+                drive.update();
 
-            } else if (gamepad1.right_bumper) {
-                Motorpower = 0.2f;
+                // Boost & Slow Modes
+                if (gamepad1.y) {
+                    Motorpower = 0.8f;
+                } else if (gamepad1.a) {
+                    Motorpower = 0.4f;
+                }
+                if (gamepad1.left_bumper) {
+                    Motorpower = 0.9f;
 
-            } else if (!gamepad1.right_bumper && !gamepad1.left_bumper) {
-                Motorpower = 0.4f;
+                } else if (gamepad1.right_bumper) {
+                    Motorpower = 0.2f;
+
+                } else if (!gamepad1.right_bumper && !gamepad1.left_bumper) {
+                    Motorpower = 0.4f;
+
+                }
+
+                Pose2d poseEstimate = drive.getPoseEstimate();
+                telemetry.addData("x", poseEstimate.getX());
+                telemetry.addData("y", poseEstimate.getY());
+                telemetry.addData("heading", poseEstimate.getHeading());
+                telemetry.update();
 
             }
-
-            Pose2d poseEstimate = drive.getPoseEstimate();
-            telemetry.addData("x", poseEstimate.getX());
-            telemetry.addData("y", poseEstimate.getY());
-            telemetry.addData("heading", poseEstimate.getHeading());
-            telemetry.update();
-
         }
     }
-}
+
