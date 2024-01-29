4b0df22196ddcbffe8c14bc569a61d1e99ade21d
Jack Cooper <105135311+JJCC523@users.noreply.github.com>
2024-01-29 15:31:15 -0500

    Create CoopControl.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/CoopControl.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.drive.teleop;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.drive.opmode.ModuleIntegrator;
+
+import java.util.Arrays;
+
+@TeleOp(name = "CoopControl")
+public class CoopControl extends LinearOpMode {
+  
+
+        float SPEED = 0.6f;
+        TeleopDrive robot;
+
+        @Override
+        public void runOpMode() {
+            robot = new TeleopDrive(hardwareMap, Arrays.asList(ModuleIntegrator.Module.CLAWTWO, ModuleIntegrator.Module.SLIDEPIVOT, ModuleIntegrator.Module.PIVOT, ModuleIntegrator.Module.DRAWERSLIDE, ModuleIntegrator.Module.CLAW, ModuleIntegrator.Module.GUN, ModuleIntegrator.Module.PIVOTTWO, ModuleIntegrator.Module.WINCHSYSTEM, ModuleIntegrator.Module.WINCHLIFTONE, ModuleIntegrator.Module.WINCHLIFTTWO), SPEED);
+            robot.slidePivot.goToPos(0.172f);
+            robot.pivottwo.setState(0.4f);
+
+            waitForStart();
+
+            while (opModeIsActive()) {
+                robot.slidePivot.move(gamepad1.right_bumper - gamepad1.left_bumper, false);
+                robot.drawerSlide.moveSlide((gamepad1.right_trigger - gamepad1.left_trigger)*0.5, false);
+
+
+                if (gamepad2.X) robot.claw.setState(0.8f);
+                if (gamepad2.X != 0) robot.claw.setState(1);
+                if (gamepad2.circle) robot.clawtwo.setState(0.8f); // close
+                if (gamepad2.circle != 0) robot.clawtwo.setState(1); // open
+                if (gamepad1.dpad_up) {robot.pivot.setState(1); robot.pivottwo.setState(1);}
+                if (gamepad1.dpad_down) {robot.pivot.setState(0.6f); robot.pivottwo.setState(0.6f);}
+                if (gamepad1.dpad_right) {robot.pivot.setState(0.9f); robot.pivottwo.setState(0.9f);}
+                if (gamepad1.dpad_left) {robot.pivot.setState(0.4f); robot.pivottwo.setState(0.4f);}
+            //    if (gamepad2.square) {robot.slidePivot.setPos(0.2f);}
+                //if (gamepad2.circle) robot.gun.setState(1);
+          //      if (gamepad2.triangle) robot.gun.setState(0);
+
+                robot.move(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+                //robot.winchSystem.move(gamepad1.right_trigger - gamepad1.left_trigger, false);
+
+              //  if (gamepad1.left_bumper) robot.setSpeed(0.2f);
+             //   if (gamepad1.right_bumper) robot.setSpeed(1);
+                if (!gamepad1.left_bumper && !gamepad1.right_bumper) robot.setSpeed(0.75f);
+               // if (gamepad1.dpad_up) {robot.winchLiftOne.setState(0.5f); }
+               // if (gamepad1.dpad_up) {robot.winchLiftTwo.setState(0.5f);}
+                //if (gamepad1.dpad_down) {robot.winchLiftOne.setState(0.15f); }
+               // if (gamepad1.dpad_down) {robot.winchLiftTwo.setState(0.15f); }
+
+
+                Pose2d poseEstimate = robot.getPoseEstimate();
+                telemetry.addData("x", poseEstimate.getX());
+                telemetry.addData("y", poseEstimate.getY());
+                telemetry.addData("heading", poseEstimate.getHeading());
+                telemetry.addData("RotationPos", robot.slidePivot.clawLift.get(0).getCurrentPosition());
+                telemetry.addData("RotationPow", robot.slidePivot.clawLift.get(0).getPower());
+                telemetry.addData("LinearslidePos", robot.drawerSlide.clawRot.get(0).getCurrentPosition());
+                telemetry.addData("LinearslidePow", robot.drawerSlide.clawRot.get(0).getPower());
+                telemetry.addData(robot.motors.get(0).getDeviceName(), robot.motors.get(0).getCurrentPosition());
+                telemetry.addData(robot.motors.get(1).getDeviceName(), robot.motors.get(1).getCurrentPosition());
+                telemetry.addData(robot.motors.get(2).getDeviceName(), robot.motors.get(2).getCurrentPosition());
+                telemetry.addData(robot.motors.get(3).getDeviceName(), robot.motors.get(3).getCurrentPosition());
+                telemetry.addData("slideJoystick", -gamepad2.right_stick_y * 0.5f);
+                telemetry.update();
+            }
+        }
+    }
